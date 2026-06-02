bac40e0a20c302f143287615e25c632c996f0e06
Sean Murphy <sean@iamseanmurphy.com>
2026-06-02 17:46:53 +0000

    3.3.9

--- gs-testimonial/templates/partials/gs-popup-content.php
+++ gs-testimonial/templates/partials/gs-popup-content.php
@@ -101,6 +101,7 @@ if (has_post_thumbnail()) {
 }
 
 
+
 if (! empty($client_name)) {
      $all_fields[] = [
         'html'     => '<h4 class="box-client-name ' . esc_attr( $get_visibility_class('gstm_reviewer_name') ) . '">' . esc_html($client_name) . '</h4>',
@@ -121,6 +122,7 @@ if (! empty($company)) {
         'position' => $get_pos('gstm_company_name'),
     ];
 }
+
 
 if (! empty($client_address)) {
     $all_fields[] = [
